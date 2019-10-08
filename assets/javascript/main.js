91ec4d0a907efca60f075c43b28d36605ec7411d
Phil Swartz <pswartz.sju@gmail.com>
2019-10-07 22:46:12 -0400

    working on contact form to send emails from emailJS service and added linkedin and github images to footer section

---
+++ assets/javascript/main.js
@@ -0,0 +1,23 @@
+
+
+
+$('#email-send-button').on('click', function() {
+
+    var fromAddress = $('#email-address').val().trim();
+    /* var subject = $('#email-subject').val().trim(); */
+    var emailMessage = $('#email-message').val().trim();
+
+
+    var template_params = {
+        "reply_to": fromAddress,
+        "from_name": "philswartz.portfolio@gmail.com",
+        "to_name": "Phil",
+        "message_html": emailMessage
+     }
+     
+     var service_id = "portfoliogmailservice";
+     var template_id = "template_6DdrviwH";
+
+     emailjs.send(service_id, template_id, template_params);
+
+});
