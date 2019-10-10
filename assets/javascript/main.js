c3a9bd01afcd100b5a6c1768bee9a1d97678f51c
Phil Swartz <pswartz.sju@gmail.com>
2019-10-09 21:35:30 -0400

    added images and updated carousel slide 1

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,22 +3,6 @@
 
 $('#email-send-button').on('click', function(e) {
     e.preventDefault();
-
-    var fromAddress = $('#email-address').val().trim();
-    /* var subject = $('#email-subject').val().trim(); */
-    var emailMessage = $('#email-message').val().trim();
-
-
-    var template_params = {
-        "reply_to": fromAddress,
-        "from_name": "philswartz.portfolio@gmail.com",
-        "to_name": "Phil",
-        "message_html": emailMessage
-     }
-     
-     var service_id = "portfoliogmailservice";
-     var template_id = "template_6DdrviwH";
-
-     emailjs.send(service_id, template_id, template_params);
+    
 
 });
